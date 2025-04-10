fe77345b91c0ea3a24adfdeac43a0e3ddbfb7f6a
A Venkata Sai Krishna Varun <varunias2004@gmail.com>
2025-04-10 18:41:54 +0530

    final changes

--- main_server.py
+++ main_server.py
@@ -3,36 +3,30 @@ import time
 import os
 import signal
 
-# Explicitly set environment variables for Node.js
-os.environ["MONGO_URI"] = "mongodb+srv://Deepika:Deepika2004@lms.3uvwm.mongodb.net/Disease?retryWrites=true&w=majority"  # Replace with your DB name
-os.environ["PORT"] = "5001"
-os.environ["JWT_SECRET"] = "dd399a2bd9e7aa3f01cde4e711f86af9480c820bb7bb0ce9ba061df52fb86d34"
+os.environ["MONGO_URI"] = "add mongo uri here"  
+os.environ["PORT"] = "give your port number here"
+os.environ["JWT_SECRET"] = "add your jwt-token here"
 
 
-# Paths to your server scripts
-BACKEND_SCRIPT = "/Users/geethadeepika/Documents/github/fsd_application/Backend/server.js"
-MODEL_SCRIPT = "/Users/geethadeepika/Documents/github/fsd_application/final_app.py"
-CHATBOT_SCRIPT = "/Users/geethadeepika/Documents/github/fsd_application/chatbot.py"
+BACKEND_SCRIPT = "./Backend/server.js"
+MODEL_SCRIPT = "/final_app.py"
+CHATBOT_SCRIPT = "/chatbot.py"
 
 def start_process(command, log_file):
     with open(log_file, "w") as log:
         return subprocess.Popen(command, stdout=log, stderr=log, shell=True, preexec_fn=os.setsid)
 
-# Start Backend (Node.js)
 print("Starting Backend Server...")
 backend_process = start_process(f"node {BACKEND_SCRIPT}", "backend.log")
-time.sleep(5)  # Give it time to initialize
+time.sleep(5) 
 
-# Start Model Server (Flask)
 print("Starting Model Server...")
 model_process = start_process(f"python3 {MODEL_SCRIPT}", "model_server.log")
 time.sleep(5)
 
-# Start Chatbot Server (Flask)
 print("Starting Chatbot Server...")
 chatbot_process = start_process(f"python3 {CHATBOT_SCRIPT}", "chatbot.log")
 
-# Keep running
 try:
     while True:
         time.sleep(1)
